3ebe74ffb4a510731c875260a50da410cd559130
sorani-dev <sorani.dev@outlook.com>
2022-02-20 22:45:23 +0100

    feat: add create planets

--- main.py
+++ main.py
@@ -12,6 +12,58 @@ pygame.display.set_caption("Planet Simulation")
 WHITE = (255, 255, 255,)
 
 
+class Planet(object):
+
+    # Approximal Unit (Distance from a planet to the sun) in meters
+    AU = 149.6e6 * 1000
+    # Gravity
+    G = 6.674258e-11
+    # Scale down (1 meter to x pixels)
+    SCALE = 250 / AU  # 1 AU = 100 pixels
+    # Time to represent in the simulation (how much time has passed since last update)
+    TIMESTEP = 3600 * 24  # One day
+
+    def __init__(self, x: float, y: float, radius: float, color: set[int, int, int], mass: float) -> None:
+        """Initialize
+
+        Args:
+            x (float): X Coordinate
+            y (float): Y Coordinate
+            radius (float): Planet radius
+            color (set[int, int, int]): Planet color
+            mass (float): Planet mass
+        """
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        # The sun does not go on an orbit
+        self.sun = False
+        # Planet's distance
+        self.distance_to_sun = 0
+
+        # Keep track of all the points this planet has travelled along
+        self.orbit: list[float] = []
+
+        # Velocity
+        self.x_velocity = 0
+        self.y_velocity = 0
+
+    def draw(self, win: pygame.Surface) -> None:
+        """Draw Planet on screen
+
+        Args:
+            win (pygame.Surface): the window to draw on
+        """
+        # Get the center of the screen
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + WIDTH / 2
+        # Draw the Planet
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+
 def main():
     """Runs the game"""
     # Is the game running?
