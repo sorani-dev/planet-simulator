1d841233bce5f59ac16825e54f8579aec1ab008a
sorani-dev <sorani.dev@outlook.com>
2022-02-20 23:53:22 +0100

    feat: add planet movement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pygame
+from __future__ import annotations
+
 import math
+
+import pygame
 
 # Game window dimensions
 WIDTH, HEIGHT = 800, 800
@@ -67,6 +70,54 @@ class Planet(object):
         # Draw the Planet
         pygame.draw.circle(win, self.color, (x, y), self.radius)
 
+    def attraction(self, other: Planet) -> tuple[float, float]:
+        """Force of attraction from a Planet to a Planet
+
+        Args:
+            other (Planet): the other Planet
+        """
+        other_x, other_y = other.x, other.y
+        # Distance between the two Planets
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x**2 + distance_y**2)
+
+        # Update the distance to the sun if the sun
+        if other.sun:
+            self.distance_to_sun = distance
+
+        force_of_attraction = self.G * self.mass * other.mass / distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force_of_attraction
+        force_y = math.sin(theta) * force_of_attraction
+        return force_x, force_y
+
+    def update_position(self, planets: list[Planet]):
+        """Update the Planets positions depending on this Planet's place
+
+        Args:
+            planets (list[Planet])
+        """
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            # Calculate the total force on x and y axis
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        # Increase velocity (F = m / a) over a period of time
+        self.x_velocity += total_fx / self.mass * self.TIMESTEP
+        self.y_velocity += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_velocity * self.TIMESTEP
+        self.y += self.y_velocity * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
+# assert Planet.__annotations__ == {'other': 'Planet'}
+
 
 def main():
     """Runs the game"""
@@ -81,15 +132,20 @@ def main():
 
     # The Earth
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_velocity = 29.783 * 1000
 
     # Mars
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_velocity = 24.077 * 1000
 
     # Mercury
     mercury = Planet(0.387*Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    mercury.y_velocity = -47.4 * 1000
 
     # Venus
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus.y_velocity = -35.02 * 1000
+
     # Planet List
     planets = [sun, earth, mars, mercury, venus]
 
@@ -97,8 +153,8 @@ def main():
         # Framerate refresh
         clock.tick(60)
 
-        # # Draw on screen
-        # WIN.fill(WHITE)
+        # # Draw background on screen
+        WIN.fill((0, 0, 0))
 
         # Manage events
         for event in pygame.event.get():
@@ -107,6 +163,7 @@ def main():
 
         # Draw Planets
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
 
         # Update display
